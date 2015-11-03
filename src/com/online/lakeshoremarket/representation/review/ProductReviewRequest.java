89691bb87f7e6404088e1c57f4306be2349bdbcd
Rayyan Jaweed <rlnu@luc.edu>
2015-11-02 20:19:37 -0600

    Added OrderRequest and ProductReviewRequest

---
+++ src/com/online/lakeshoremarket/representation/review/ProductReviewRequest.java
@@ -0,0 +1,45 @@
+package com.online.lakeshoremarket.representation.review;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "ProductReview")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+public class ProductReviewRequest {
+
+	private int customerID;
+	private int productID;
+	private int rating;
+	private String review;
+	
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	
+	
+}
