2fb08b762269c12ed6ed5134b6f1e531eaa8d59d
mlaust <maggie.aust@gmail.com>
2015-11-02 22:43:03 -0600

    Updated deleteCustomer success and error messages (generic)

--- src/com/online/lakeshoremarket/service/LakeshoreMarketResource.java
+++ src/com/online/lakeshoremarket/service/LakeshoreMarketResource.java
@@ -281,16 +281,20 @@ public class LakeshoreMarketResource {
 	@DELETE
 	@Produces({"application/xml" , "application/json"})
 	@Path("/customer/{customerID}")
-	public Response deleteCustomer(@PathParam("customerID") String customerIDString){
+	public GenericResponse deleteCustomer(@PathParam("customerID") String customerIDString){
 		System.out.println("DELETE METHOD Request for Deleting a Customer ............." + customerIDString);
 		boolean isCustomerDeleted = false;
 		LakeshoreMarketActivity marketActivity = new LakeshoreMarketActivity();
 		isCustomerDeleted = marketActivity.deleteCustomer(customerIDString);
+		GenericResponse genericResponse = new GenericResponse();
 		if(isCustomerDeleted){
-			return Response.status(Response.Status.OK).build();
+			genericResponse.setMessage("Customer has been deleted");
+			genericResponse.setSuccess(true);
 		}else{
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
-		}
+			genericResponse.setMessage("Customer was not deleted");
+			genericResponse.setSuccess(false);
+		}		
+		return genericResponse;
 		
 	}
 	
