d750f72459cab3f4b16377b8e7646c99a043a1a7
Rayyan Jaweed <rlnu@luc.edu>
2015-11-03 00:51:13 -0600

    Added exception handling with response codes and messages for ReviewDAO

--- src/com/online/lakeshoremarket/dao/ReviewDAO.java
+++ src/com/online/lakeshoremarket/dao/ReviewDAO.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.ws.rs.core.Response;
+
+import com.online.lakeshoremarket.exception.GenericLSMException;
 import com.online.lakeshoremarket.model.review.Review;
 import com.online.lakeshoremarket.model.review.ReviewImpl;
 import com.online.lakeshoremarket.util.DatabaseConnection;
@@ -39,14 +42,18 @@ public class ReviewDAO {
 			pstmt.setTimestamp(5, review.getReviewDate());
 			rowsUpdated = pstmt.executeUpdate();
 		}catch(SQLException sqe){
-			System.err.println("ReviewDAO.addPartnerReview: Threw a SQLException inserting a new partner review in table.");
+			System.err.println("ReviewDAO.addPartnerReview: Threw an SQLException inserting a new partner review in table.");
   	      	System.err.println(sqe.getMessage());
+  	      	throw new GenericLSMException("Threw an SQLException inserting a new partner review in table.		" 
+					+ sqe.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 		} finally {
 			try {
 				pstmt.close();
 				conn.close();
 			} catch (Exception e) {
 				System.err.println("ReviewDAO.addPartnerReview: Threw an Exception inserting a new partner review in table.");
+				throw new GenericLSMException("Threw an Exception inserting a new partner review in table.		" 
+						+ e.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 			}
 		}
 		return (rowsUpdated == 0) ? false : true ;
@@ -73,14 +80,18 @@ public class ReviewDAO {
 			pstmt.setTimestamp(5, review.getReviewDate());
 			rowsUpdated = pstmt.executeUpdate();
 		}catch(SQLException sqe){
-			System.err.println("ReviewDAO.addProductReview: Threw a SQLException inserting a new product review in table.");
+			System.err.println("ReviewDAO.addProductReview: Threw an SQLException inserting a new product review in table.");
   	      	System.err.println(sqe.getMessage());
+  	      	throw new GenericLSMException("Threw an SQLException inserting a new product review in table.		" 
+					+ sqe.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 		} finally {
 			try {
 				pstmt.close();
 				conn.close();
 			} catch (Exception e) {
 				System.err.println("ReviewDAO.addProductReview: Threw an Exception inserting a new product review in table.");
+				throw new GenericLSMException("Threw an Exception inserting a new product review in table.		" 
+						+ e.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 			}
 		}
 		return (rowsUpdated == 0) ? false : true ;
@@ -111,14 +122,18 @@ public class ReviewDAO {
 			}
 			
 		}catch(SQLException sqe){
-			System.err.println("ReviewDAO.getPartnerReviewByID: Threw a SQLException getting partner review from table.");
+			System.err.println("ReviewDAO.getPartnerReviewByID: Threw an SQLException getting partner review from table.");
   	      	System.err.println(sqe.getMessage());
+  	      	throw new GenericLSMException("Threw an SQLException getting partner review from table.		" 
+					+ sqe.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 		} finally {
 			try {
 				pstmt.close();
 				conn.close();
 			} catch (Exception e) {
-				System.err.println("ReviewDAO.getPartnerReviewByID: Threw a SQLException getting partner review from table.");
+				System.err.println("ReviewDAO.getPartnerReviewByID: Threw an Exception getting partner review from table.");
+				throw new GenericLSMException("Threw an Exception getting partner review from table.		" 
+						+ e.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 			}
 		}
 		
@@ -151,14 +166,18 @@ public class ReviewDAO {
 			}
 			
 		}catch(SQLException sqe){
-			System.err.println("ReviewDAO.getPartnerReviewByID: Threw a SQLException getting product review from table.");
+			System.err.println("ReviewDAO.getPartnerReviewByID: Threw an SQLException getting product review from table.");
   	      	System.err.println(sqe.getMessage());
+  	      	throw new GenericLSMException("Threw an SQLException getting product review from table.		" 
+					+ sqe.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 		} finally {
 			try {
 				pstmt.close();
 				conn.close();
 			} catch (Exception e) {
-				System.err.println("ReviewDAO.getPartnerReviewByID: Threw a SQLException getting product review from table.");
+				System.err.println("ReviewDAO.getPartnerReviewByID: Threw an Exception getting product review from table.");
+				throw new GenericLSMException("Threw an Exception getting product review from table.		" 
+						+ e.getMessage() , Response.Status.INTERNAL_SERVER_ERROR );
 			}
 		}
 		
