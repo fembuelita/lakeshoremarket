031040ac4ff251657819ae86c6405410ab4c211a
Elly Post <elly@ellytronic.media>
2015-12-08 02:05:36 -0600

    Temporary disconnect of standard order calls for testing

--- src/com/online/lakeshoremarket/resource/OrderResource.java
+++ src/com/online/lakeshoremarket/resource/OrderResource.java
@@ -26,79 +26,6 @@ import com.online.lakeshoremarket.util.LSMAuthenticator;
  */
 
 public class OrderResource {
-
-	/**
-	 * DELETE method for canceling an order
-	 * @param orderIDString
-	 * @param email
-	 * @param password
-	 * @return generic response (if user is able to cancel)-- either affirmative or negative depending on status; else, exception message
-	 */
-	
-	@DELETE
-	@Produces({"application/xml" , "application/json"})
-	@Path("/order/{orderIDString}")
-	public GenericResponse cancelOrder(@PathParam("orderIDString") String orderIDString, @HeaderParam("email") String email, @HeaderParam("password") String password){
-		System.out.println("DELETE METHOD Request for Canceling an order .............");
-		boolean isUserAuthentic = false;
-		isUserAuthentic = LSMAuthenticator.authenticateUser(email, password);
-		if(isUserAuthentic){
-			GenericResponse genericResponse = new GenericResponse();
-			boolean isOrderRefunded = false;
-			OrderActivity orderActivity = new OrderActivity();
-			isOrderRefunded = orderActivity.cancelOrder(orderIDString);
-			if(isOrderRefunded){
-				genericResponse.setMessage("Order is cancelled");
-				genericResponse.setSuccess(true);
-				Link get = new Link("Get Order Details", Constant.LSM_COMMON_URL + "/order/" + orderIDString, "application/xml");
-				genericResponse.setLinks(get);
-			}else{
-				genericResponse.setMessage("Order is not cancelled");
-				genericResponse.setSuccess(false);
-			}
-			return genericResponse;
-		}else{
-			throw new GenericLSMException("User is not authorized", Response.Status.UNAUTHORIZED);
-		}
-		
-	}
-	
-	/**
-	 * GET method request for order details
-	 * @param orderIDString
-	 * @param email
-	 * @param password
-	 * @return orderRepresentation (if user is authorized to do so)
-	 */
-	
-	@GET
-	@Produces({"application/xml" , "application/json"})
-	@Path("/order/{orderIDString}")
-	public OrderRepresentation getOrderDetails(@PathParam("orderIDString") String orderIDString, @HeaderParam("email") String email, @HeaderParam("password") String password){
-		System.out.println("GET METHOD Request for Order details.............");
-		boolean isUserAuthentic = false;
-		isUserAuthentic = LSMAuthenticator.authenticateUser(email, password);
-		if(isUserAuthentic){
-			OrderActivity orderActivity = new OrderActivity();
-			OrderRepresentation orderRepresentation = new OrderRepresentation();
-			orderRepresentation = orderActivity.getOrderDetails(orderIDString);
-			
-			Link cancel = new Link("Cancel Order", Constant.LSM_COMMON_URL + "/order/" + orderRepresentation.getOrderID(), "application/xml");
-			Link ship = new Link("Ship Order", Constant.LSM_COMMON_URL + "/order/ship/" + orderRepresentation.getOrderID(), "application/xml");
-			Link fulfill = new Link("Fulfill Order", Constant.LSM_COMMON_URL + "/order/fulfill/"+orderRepresentation.getOrderID() , "application/xml");
-			// If order is shipped, we can further proceed for fulfilling the order
-			if( orderRepresentation.getOrderStatusCode() == Constant.SHIPPED )
-				orderRepresentation.setLinks( cancel, fulfill );
-			
-			//If order is in progress, we can further proceed for shipping the order 
-			if(orderRepresentation.getOrderStatusCode() == Constant.INPROGRESS )
-				orderRepresentation.setLinks( cancel, ship );
-			
-			return orderRepresentation;
-		}else{
-			throw new GenericLSMException("User is not authorized", Response.Status.UNAUTHORIZED);
-		}
-	}
 	
 	/**
 	 * POST method to ship order
@@ -173,4 +100,77 @@ public class OrderResource {
 			throw new GenericLSMException("User is not authorized", Response.Status.UNAUTHORIZED);
 		}
 	}
+	
+	/**
+	 * DELETE method for canceling an order
+	 * @param orderIDString
+	 * @param email
+	 * @param password
+	 * @return generic response (if user is able to cancel)-- either affirmative or negative depending on status; else, exception message
+	 */
+	
+	/*@DELETE
+	@Produces({"application/xml" , "application/json"})
+	@Path("/order/{orderIDString}")
+	public GenericResponse cancelOrder(@PathParam("orderIDString") String orderIDString, @HeaderParam("email") String email, @HeaderParam("password") String password){
+		System.out.println("DELETE METHOD Request for Canceling an order .............");
+		boolean isUserAuthentic = false;
+		isUserAuthentic = LSMAuthenticator.authenticateUser(email, password);
+		if(isUserAuthentic){
+			GenericResponse genericResponse = new GenericResponse();
+			boolean isOrderRefunded = false;
+			OrderActivity orderActivity = new OrderActivity();
+			isOrderRefunded = orderActivity.cancelOrder(orderIDString);
+			if(isOrderRefunded){
+				genericResponse.setMessage("Order is cancelled");
+				genericResponse.setSuccess(true);
+				Link get = new Link("Get Order Details", Constant.LSM_COMMON_URL + "/order/" + orderIDString, "application/xml");
+				genericResponse.setLinks(get);
+			}else{
+				genericResponse.setMessage("Order is not cancelled");
+				genericResponse.setSuccess(false);
+			}
+			return genericResponse;
+		}else{
+			throw new GenericLSMException("User is not authorized", Response.Status.UNAUTHORIZED);
+		}
+		*/
+	}
+	
+	/**
+	 * GET method request for order details
+	 * @param orderIDString
+	 * @param email
+	 * @param password
+	 * @return orderRepresentation (if user is authorized to do so)
+	 */
+	/*
+	@GET
+	@Produces({"application/xml" , "application/json"})
+	@Path("/order/{orderIDString}")
+	public OrderRepresentation getOrderDetails(@PathParam("orderIDString") String orderIDString, @HeaderParam("email") String email, @HeaderParam("password") String password){
+		System.out.println("GET METHOD Request for Order details.............");
+		boolean isUserAuthentic = false;
+		isUserAuthentic = LSMAuthenticator.authenticateUser(email, password);
+		if(isUserAuthentic){
+			OrderActivity orderActivity = new OrderActivity();
+			OrderRepresentation orderRepresentation = new OrderRepresentation();
+			orderRepresentation = orderActivity.getOrderDetails(orderIDString);
+			
+			Link cancel = new Link("Cancel Order", Constant.LSM_COMMON_URL + "/order/" + orderRepresentation.getOrderID(), "application/xml");
+			Link ship = new Link("Ship Order", Constant.LSM_COMMON_URL + "/order/ship/" + orderRepresentation.getOrderID(), "application/xml");
+			Link fulfill = new Link("Fulfill Order", Constant.LSM_COMMON_URL + "/order/fulfill/"+orderRepresentation.getOrderID() , "application/xml");
+			// If order is shipped, we can further proceed for fulfilling the order
+			if( orderRepresentation.getOrderStatusCode() == Constant.SHIPPED )
+				orderRepresentation.setLinks( cancel, fulfill );
+			
+			//If order is in progress, we can further proceed for shipping the order 
+			if(orderRepresentation.getOrderStatusCode() == Constant.INPROGRESS )
+				orderRepresentation.setLinks( cancel, ship );
+			
+			return orderRepresentation;
+		}else{
+			throw new GenericLSMException("User is not authorized", Response.Status.UNAUTHORIZED);
+		}
+	}*/	
 }
