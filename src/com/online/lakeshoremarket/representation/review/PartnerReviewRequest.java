1823d9eac93c04089d08ccf4cb0986205bbc0341
Maggie Aust <maggieaust@Maggies-MacBook-Pro.local>
2015-12-07 21:13:54 -0600

    added documentation for the PartnerReviewRequest class

--- src/com/online/lakeshoremarket/representation/review/PartnerReviewRequest.java
+++ src/com/online/lakeshoremarket/representation/review/PartnerReviewRequest.java
@@ -22,27 +22,74 @@ public class PartnerReviewRequest {
 	private int rating;
 	private String review;
 	
+	/**
+	 * gets the customer ID of the reviewer
+	 * @return the id
+	 */
+	
 	public int getCustomerID() {
 		return customerID;
 	}
+	
+	/**
+	 * sets the reviewer customer ID
+	 * @param customerID the id
+	 */
+	
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
+	
+	/**
+	 * gets the ID of the partner this review is for, null if for product
+	 * @return the id or null
+	 */
+	
 	public int getPartnerID() {
 		return partnerID;
 	}
+	
+	/**
+	 * sets the ID of the partner this review is for
+	 * @param partnerID the id 
+	 */
+	
 	public void setPartnerID(int partnerID) {
 		this.partnerID = partnerID;
 	}
+	
+	/**
+	 * gets the review rating (eg: 1-5)
+	 * @return the rating
+	 */
+	
 	public int getRating() {
 		return rating;
 	}
+	
+	/**
+	 * sets the review rating
+	 * @param rating the rating
+	 */
+	
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+	
+	/**
+	 * gets the review text
+	 * @return the review text
+	 */
+	
 	public String getReview() {
 		return review;
 	}
+	
+	/**
+	 * sets the review text
+	 * @param review the text
+	 */
+	
 	public void setReview(String review) {
 		this.review = review;
 	}
