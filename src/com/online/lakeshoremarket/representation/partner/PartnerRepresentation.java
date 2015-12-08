1d2f34de6e770d40c8e51dd348ab9f2d472feecc
Maggie Aust <maggieaust@Maggies-MacBook-Pro.local>
2015-12-07 19:13:02 -0600

    added documentation for partner representation.

--- src/com/online/lakeshoremarket/representation/partner/PartnerRepresentation.java
+++ src/com/online/lakeshoremarket/representation/partner/PartnerRepresentation.java
@@ -8,6 +8,12 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name = "Partner")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "")
+
+/**
+ * This class builds the representation of partners that interact with and are manipulated by the user in the service layer.
+ *
+ */
+
 public class PartnerRepresentation {
 
 	private boolean isActive;
@@ -19,51 +25,146 @@ public class PartnerRepresentation {
 	private String password;
 	private String phone;
 	
+	/**
+	 * determines if a partner representation is active or not
+	 * @return true if active, false else
+	 */
+	
 	public boolean isActive() {
 		return isActive;
 	}
+	
+	/**
+	 * sets a partner representation's active status
+	 * @param isActive true or false
+	 */
+	
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
+	
+	/**
+	 * gets an address ID for the partner representation
+	 * @return 	the address ID
+	 */
+	
 	public int getAddressID() {
 		return addressID;
 	}
+	
+	/**
+	 * sets an address ID for the partner representation
+	 * @param billingAddress	the address ID
+	 */
+	
 	public void setAddressID(int addressID) {
 		this.addressID = addressID;
 	}
+	
+	/**
+	 * gets a partner ID for the partner representation
+	 * @return 	the partner ID
+	 */
+	
 	public int getPartnerID() {
 		return partnerID;
 	}
+	
+	/**
+	 * sets a partner ID for the partner representation
+	 * @param 	the partner ID
+	 */
+	
 	public void setPartnerID(int partnerID) {
 		this.partnerID = partnerID;
 	}
+	
+	/**
+	 * gets the partner's contact name
+	 * @return the contact name
+	 */
+	
 	public String getContactName() {
 		return contactName;
 	}
+	
+	/**
+	 * sets the partner's contact name
+	 * @param contactName 	the contact name
+	 */
+	
 	public void setContactName(String contactName) {
 		this.contactName = contactName;
 	}
+	
+	/**
+	 * gets the partner's email address
+	 * @return 	the email
+	 */
+	
 	public String getEmail() {
 		return email;
 	}
+	
+	/**
+	 * sets the partner's email
+	 * @param email the email
+	 */
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	/**
+	 * gets the partner's name
+	 * @return the name
+	 */
+	
 	public String getPartnerName() {
 		return partnerName;
 	}
+	
+	/**
+	 * sets the partner name
+	 * @param partnerName 	the name
+	 */
+	
 	public void setPartnerName(String partnerName) {
 		this.partnerName = partnerName;
 	}
+	
+	/**
+	 * gets the password (hashed)
+	 * @return the password
+	 */
+	
 	public String getPassword() {
 		return password;
 	}
+	
+	/**
+	 * sets the password
+	 * @param password the password
+	 */
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	/**
+	 * gets the partner's phone
+	 * @return the phone
+	 */
+	
 	public String getPhone() {
 		return phone;
 	}
+	
+	/**
+	 * sets the partner's phone
+	 * @param phone the phone
+	 */
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
