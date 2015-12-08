f275b2c133b8f87f5b251e5b862434d71b3e895b
Elly Post <elly@ellytronic.media>
2015-12-07 21:10:42 -0600

    Added an authenticate customer method in addition to authenticate user

--- src/com/online/lakeshoremarket/resource/UtilResource.java
+++ src/com/online/lakeshoremarket/resource/UtilResource.java
@@ -21,7 +21,7 @@ public class UtilResource {
 	@Produces({"application/xml" , "application/json"})
 	@Path("/authenticate")
 	public GenericResponse authenticate( @HeaderParam("email") String email, @HeaderParam("password") String password){
-		System.out.println("AUTHENTICATE METHOD Request for customer .............");
+		System.out.println("AUTHENTICATE METHOD Request for user .............");
 		boolean isUserAuthentic = false;
 		isUserAuthentic = LSMAuthenticator.authenticateUser(email, password);
 		if(isUserAuthentic){
@@ -33,4 +33,27 @@ public class UtilResource {
 		
 		throw new GenericLSMException("User is not authenticated", Response.Status.UNAUTHORIZED);
 	}
+	
+	
+	/**
+	 * Authenticates a customer and returns the customer ID as part of the detail
+	 * @param email
+	 * @param password
+	 * @return
+	 */
+	@GET
+	@Produces({"application/xml" , "application/json"})
+	@Path("/authenticate/customer")
+	public GenericResponse authenticateCustomer( @HeaderParam("email") String email, @HeaderParam("password") String password){
+		System.out.println("AUTHENTICATE METHOD Request for customer .............");
+		int customerID = LSMAuthenticator.authenticateCustomer(email, password);
+		if(customerID < 1)
+			throw new GenericLSMException("User is not authenticated", Response.Status.UNAUTHORIZED);
+	
+		GenericResponse genericResponse = new GenericResponse();
+		genericResponse.setMessage("User is authenticated");
+		genericResponse.setSuccess(true);
+		genericResponse.setGenericReturnValue( Integer.toString( customerID ) );
+		return genericResponse;
+	}	
 }
--- src/com/online/lakeshoremarket/util/LSMAuthenticator.java
+++ src/com/online/lakeshoremarket/util/LSMAuthenticator.java
@@ -70,4 +70,48 @@ public class LSMAuthenticator {
 		
 		return isUserAuthentic;
 	}
+	
+	/**
+	 * verifies a customer exists
+	 * @param email 	the customer email
+	 * @param password	the customer password hash
+	 * @return customerID 	the customer ID from DB or -1 if invalid 
+	 */
+	public static int authenticateCustomer(String email, String password) {
+		int customerID = -1;
+		Connection conn = DatabaseConnection.getSqlConnection();
+		PreparedStatement pstmt = null;
+		
+		if(conn != null){
+			try{
+				String searchQuery = "SELECT * FROM `customer` WHERE email = ? AND password = ? AND active = 1 LIMIT 1";
+				pstmt = conn.prepareStatement(searchQuery);
+				pstmt.setString(1, email);
+				pstmt.setString(2, password);
+				ResultSet resultSet = pstmt.executeQuery();
+				while(resultSet.next()){
+					int custId = resultSet.getInt("customer_id");
+					if(custId >= 0)
+						customerID = custId;
+					return customerID;
+				}
+				
+			}catch(SQLException sqe){
+				System.err.println("Threw an SQLException while authenticating user.");
+	  	      	System.err.println(sqe.getMessage());
+			} finally {
+				try {
+					pstmt.close();
+					conn.close();
+				} catch (Exception e) {
+					System.err.println("Threw an SQLException while authenticating user.");
+					System.err.println(e.getMessage());
+				}
+			}
+		}else{
+			throw new GenericLSMException("Failed to establish connection with the DB", Response.Status.INTERNAL_SERVER_ERROR);
+		}
+		
+		return customerID;
+	}	
 }
